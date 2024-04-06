332b71c4d4f907078659a60fee5688e7db6b5fad
Matt <46137770+xMdb@users.noreply.github.com>
2024-04-06 13:15:45 +1000

    Update CreateScriptTask.ps1

--- CreateScriptTask.ps1
+++ CreateScriptTask.ps1
@@ -1,3 +1,3 @@
 $action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-ExecutionPolicy Bypass -Command "powershell -ExecutionPolicy Bypass -File $env:USERPROFILE\Desktop\MattOOBE.ps1"'
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(2)
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(3)
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "MattOOBE" -Description "Run MattOOBE after logon" -Force
