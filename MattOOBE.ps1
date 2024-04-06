cf101bdd704c82e1dc7e20b5ef0eb5f81791329d
Matt <46137770+xMdb@users.noreply.github.com>
2024-04-06 13:44:15 +1000

    Update MattOOBE.ps1

--- MattOOBE.ps1
+++ MattOOBE.ps1
@@ -25,17 +25,14 @@ if ($null -eq $virtioPath) {
     Start-Process -FilePath $virtioPath.FullName -ArgumentList "/quiet /norestart" -Wait
 }
 
-Write-Host "Done." -ForegroundColor Green
-Write-Host ""
-
 Write-Host "Enabling Remote Desktop connections..."
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -Name fDenyTSConnections -Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
 Write-Host "Setting the VNC resolution to 1920x1080..."
 Install-PackageProvider -Name NuGet -Force -MinimumVersion 2.8.5.201 | Out-Null
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module -Name ChangeScreenResolution -Scope CurrentUser -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted | Out-Null
+Install-Module -Name ChangeScreenResolution -Scope CurrentUser -Force | Out-Null
 Import-Module ChangeScreenResolution
 Set-ScreenResolution -Width 1920 -Height 1080
 
@@ -43,7 +40,7 @@ Write-Host "Grabbing the Microsoft Activation Script and activating..."
 & ([ScriptBlock]::Create((Invoke-RestMethod https://massgrave.dev/get))) /HWID
 
 Write-Host "Removing OneDrive and Edge..."
-Start-Process "$env:windir\System32\OneDriveSetup.exe" "/uninstall"
+Start-Process "$env:windir\System32\OneDriveSetup.exe" "/uninstall" -ErrorAction SilentlyContinue
 (New-Object System.Net.WebClient).DownloadFile("https://github.com/ShadowWhisperer/Remove-MS-Edge/blob/main/Remove-EdgeOnly.exe?raw=true", "$env:TEMP\Remove-EdgeOnly.exe")
 Start-Process -FilePath "$env:TEMP\Remove-EdgeOnly.exe" -Wait
 
@@ -62,6 +59,10 @@ winget install LibreWolf.LibreWolf 7zip.7zip VideoLAN.VLC 9PF4KZ2VN4W9 9MSMLRH6L
 
 Write-Host "Setting LibreWolf as the default browser..."
 Start-Process "$env:programfiles\LibreWolf\librewolf.exe" "-setDefaultBrowser"
+
+Write-Host "Opening TranslucentTB for auto-start..."
+$familyName = (Get-AppxPackage *TranslucentTB*).PackageFamilyName
+Start-Process explorer.exe "shell:appsFolder\$familyName!TranslucentTB"
 
 Write-Host "Completed automatic setup." -ForegroundColor Green
 Write-Host ""
@@ -96,7 +97,7 @@ Write-Host "Removing Powershell NuGet provider and DisplaySettings module..."
 (Get-PackageProvider NuGet).ProviderPath | Remove-Item -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "$env:ProgramFiles\PackageManagement\ProviderAssemblies\nuget\*" -Force -Recurse -ErrorAction SilentlyContinue
 Set-PSRepository -Name PSGallery -InstallationPolicy Untrusted
-Remove-Module DisplaySettings -Force
+Remove-Module ChangeScreenResolution -Force
 Remove-Item -Path "$env:USERPROFILE\Documents\WindowsPowerShell" -Force -Recurse -ErrorAction SilentlyContinue
 
 Write-Host "Removing temporary files..."
